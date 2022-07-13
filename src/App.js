8bc6a606b87e28fcc13d2420877137f25c567950
prakash <prakashjaiswal625@gmail.com>
2022-07-13 12:16:06 +0530

    deploy my react app to github pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styled from "styled-components";
+import Axios from "axios";
+
+import CityComponent from "./modules/CityComponent";
+import WeatherComponent from "./modules/WeatherInfoComponent";
+
+export const WeatherIcons = {
+ "01d": "react-deploy/icons/01d@2x.png",
+  "01n": "react-deploy/icons/01n@2x.png",
+  "02d": "react-deploy/icons/02d@2x.png",
+  "02n": "react-deploy/icons/02n@2x.png",
+  "03n": "react-deploy/icons/03n@2x.png",
+  "03d": "react-deploy/icons/03d@2x.png",
+  "04d": "react-deploy/icons/04d@2x.png",
+  "05d": "react-deploy/icons/03d@2x.png",
+  "50n": "react-deploy/icons/03d@2x.png",
+  "04n": "react-deploy/icons/03d@2x.png",
+  "09n": "react-deploy/icons/09n@2x.png",
+  "09d": "react-deploy/icons/09d@2x.png",
+  "10d": "react-deploy/icons/10d@2x.png",
+  "10n": "react-deploy/icons/10n@2x.png",
+  "11d": "react-deploy/icons/11d@2x.png",
+  "11n": "react-deploy/icons/11n@2x.png",
+  "13n": "react-deploy/icons/13n@2x.png",
+  "13d": "react-deploy/icons/13d@2x.png",
+  "50d": "react-deploy/icons/50d@2x.png",
+};
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 380px;
+  padding: 20px 10px 10px 20px;
+  margin: auto;
+  border-radius: 4px;
+  box-shadow: 0 3px 6px 0 #555;
+  background: white;
+  font-family: Montserrat;
+`;
+
+const AppLabel = styled.span`
+  color: black;
+  margin: 20px auto;
+  font-size: 18px;
+  font-weight: bold;
+`;
+
 
 function App() {
+  const[city,updateCity] = useState();
+  const[weather,updateWeather] = useState();
+  const fetchWeather = async(e) => {
+    e.preventDefault();
+    const response = await Axios.get(
+         ` https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=389ab5f855b1635a19596ecdf07f2e0d`,
+    );
+    updateWeather(response.data);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+
+      <AppLabel><h2> React App </h2> </AppLabel>
+       {city && weather ? (
+        <WeatherComponent weather = {weather } city = {city} />
+       ): (
+        <CityComponent updateCity = { updateCity} fetchWeather = { fetchWeather} />
+       ) }
+      
+      
+     
+    </Container>
   );
 }
 
---
+++ src/modules/CityComponent.js
@@ -0,0 +1,65 @@
+import React from "react";
+import styled from "styled-components";
+
+
+const SearchBox = styled.form`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  margin: 20px;
+  border: black solid 1px;
+  border-radius: 2px;
+  & input{
+    padding: 10px;
+    font-size: 15px;
+    border: none;
+    outline: none;
+    font-family: Montserrat;
+    font-weight: bold;
+  
+  }
+  & button{
+    background-color: teal;
+    font-size: 16px;
+    padding: 0 10px;
+    color: white;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    font-family: Montserrat;
+    font-weight: bold;
+  }
+  `;
+
+  const ChooseCityLabel = styled.span`
+  color: black;
+  margin: 10px auto;
+  font-size: 15px;
+  font-weight: bold;
+`;
+const WeatherLogo = styled.img`
+  width: 140px;
+  height: 140px;
+  margin: 40px auto;
+`;
+
+const CityComponent = (props) => {
+    const{updateCity , fetchWeather} = props;
+    return (
+        <>
+        <WeatherLogo src = "react-deploy/icons/logo.svg" />
+        < ChooseCityLabel > Find Weather of your City </ChooseCityLabel>
+        <SearchBox  onSubmit={fetchWeather}> 
+            <input 
+              onChange= {(e) => updateCity(e.target.value)}
+            placeholder=" City"
+            /> 
+         <button type = {"submit"}>
+             Search
+            </button>  
+             </SearchBox>
+        </>
+    );
+};
+
+export default CityComponent;
